71ef4b08787b8583b1441e1dda35ce9b1ea30fb8
Tobse <Tobse4Git@gmail.com>
2020-01-03 16:15:25 +0100

    🔨 Shorten MainView code

--- src/main/java/org/cleancode/journal/view/MainView.java
+++ src/main/java/org/cleancode/journal/view/MainView.java
@@ -10,31 +10,25 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.material.Material;
-import org.cleancode.journal.component.SingleBreadcrumb;
 
-//@PreserveOnRefresh
 @CssImport("./styles/shared-styles.css")
 @Theme(value = Material.class, variant = Lumo.DARK)
-@PWA(name = "Clean Code Developer Journal", shortName = "Clean Code Journal", backgroundColor = "#3B3B3B")
+@PWA(name = "Clean Code Developer Journal", shortName = "Clean Code Journal")
 public class MainView extends AppLayout {
 
     public MainView() {
-
         addToNavbar(new DrawerToggle());
-        addToNavbar(new H4(getTranslation("app.name")));
-        addToNavbar(new SingleBreadcrumb());
-
+        addToNavbar(new H4("Clean Code Journal"));
         addToDrawer(createMenuBar());
     }
 
     private VerticalLayout createMenuBar() {
-        final RouterLink journal = new RouterLink(getTranslation("app.menu.journal"), JournalView.class);
-        final RouterLink profile = new RouterLink(getTranslation("app.menu.profile"), ProfileView.class);
-        final RouterLink achievements = new RouterLink(getTranslation("app.menu.achievements"), AchievementsView.class);
-        final RouterLink compendium = new RouterLink(getTranslation("app.menu.compendium"), CompendiumView.class);
-        final RouterLink about = new RouterLink(getTranslation("app.menu.about"), AboutView.class);
+        var journal = new RouterLink("Journal", JournalView.class);
+        var profile = new RouterLink("Profil", ProfileView.class);
+        var achievements = new RouterLink("Erfolge", AchievementsView.class);
+        var compendium = new RouterLink("Kompendium", CompendiumView.class);
+        var about = new RouterLink("Über", AboutView.class);
         return new VerticalLayout(journal, profile, achievements, compendium, about);
     }
-
 
 }
